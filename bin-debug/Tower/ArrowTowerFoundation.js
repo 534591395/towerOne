7fa4822f11036daf9a51907622e0317ba845ff83
hzwangyang1 <hzwangyang1@corp.netease.com>
2019-11-01 09:00:03 +0800

    【MOD】修复隐藏建筑tool失败问题。

---
+++ bin-debug/Tower/ArrowTowerFoundation.js
@@ -0,0 +1,83 @@
+/**
+ * 箭塔基类-箭塔的地基
+ */
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = this && this.__extends || function __extends(t, e) { 
+ function r() { 
+ this.constructor = t;
+}
+for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i]);
+r.prototype = e.prototype, t.prototype = new r();
+};
+var ArrowTowerFoundation = (function (_super) {
+    __extends(ArrowTowerFoundation, _super);
+    function ArrowTowerFoundation() {
+        var _this = _super.call(this) || this;
+        /** 先左边攻击还是先右边攻击 */
+        _this.firststrike = "R";
+        /**开火延迟*/
+        _this.fireDelay = 1000;
+        /**时间累计*/
+        _this.timesum = 0;
+        /**音效资源*/
+        _this.voiceArr = ["arrow_ready1", "arrow_ready2", "arrow_ready3"];
+        //播放音效
+        var idx = Math.floor(Math.random() * 3);
+        return _this;
+        // SoundManager.playEffect(this.voiceArr[idx]);
+    }
+    /**播放射击音效*/
+    ArrowTowerFoundation.prototype.playFireVoice = function () {
+        if (Math.random() > 0.5) {
+            SoundManager.playEffect("arrow_fire1");
+        }
+        else {
+            SoundManager.playEffect("arrow_fire2");
+        }
+    };
+    // 帧率执行回调方法
+    ArrowTowerFoundation.prototype.onEnterFrame = function (timeStamp) {
+        var _this = this;
+        // 进入攻击范围的敌人，没有攻击对象的敌人
+        this.atargets = [];
+        this.targets.map(function (item) {
+            if (item.target === null) {
+                var isIn = Utiles.containsXY(item.x, item.y, _this.sx, _this.sy - 22, _this.maxRadius, _this.ratioY);
+                var index = _this.atargets.indexOf(item);
+                if (isIn && item.hp > 0) {
+                    if (index === -1) {
+                        _this.atargets.push(item);
+                    }
+                }
+                else {
+                    // 已经不在该塔的攻击范围内
+                    if (index > -1) {
+                        _this.atargets.splice(index, 1);
+                    }
+                }
+            }
+        });
+        //排序、敌人的攻击对象不为null时则排到末尾
+        this.targets.map(function (item) {
+            if (item.target !== null) {
+                var isIn = Utiles.containsXY(item.x, item.y, _this.sx, _this.sy - 22, _this.maxRadius, _this.ratioY);
+                var index = _this.atargets.indexOf(item);
+                if (isIn && item.hp > 0) {
+                    if (index === -1) {
+                        _this.atargets.push(item);
+                    }
+                }
+                else {
+                    // 已经不在该塔的攻击范围内
+                    if (index > -1) {
+                        _this.atargets.splice(index, 1);
+                    }
+                }
+            }
+        });
+    };
+    return ArrowTowerFoundation;
+}(TowerFoundation));
+__reflect(ArrowTowerFoundation.prototype, "ArrowTowerFoundation");
